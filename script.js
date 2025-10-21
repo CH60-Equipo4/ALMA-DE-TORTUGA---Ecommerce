e7febf44db0a4797c24c29c838f3c7f10d0472ed
rosaainz <rosagsainzz@gmail.com>
2025-10-21 01:23:07 -0600

    feat(regex): implementar regex para la validacion del correo electronico y telefono en formulario de contacto

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form');
+const message = document.getElementById('message');
+
+const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+const phoneRegex = /^\d{10}$/; //^\+?52?\s?\d{10}$
+
+form.addEventListener('submit', function(event)){
+    event.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+
+    if( !emailRegex.test(email) ) {
+        message.textContent = 'Por favor escribe una dirección de correo válida.';
+        message.style.color = 'red';
+        return;
+    }
+
+    if( !phoneRegex.test(phone) ) {
+        message.textContent = 'Por favor escribe un número de teléfono válido de 10 dígitos.';
+        message.style.color = 'red';
+        return;
+    }
+
+    message.textContent = "Formulario válido, enviado con éxito.";
+    message.style.color = 'green';
+}
