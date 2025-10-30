df3d42b808f27b94edf663addce9ad91bc67cb15
zepahua25 <jdaniel.zepahua@gmail.com>
2025-10-30 17:50:16 -0600

    ADTEC-43-Update Paths
    Acrtualizacion de paths de nueva estructura

--- Pages/Totebags/itemsController.js
+++ Pages/Totebags/itemsController.js
@@ -37,74 +37,74 @@ itemsController.addItem(
     "Squirtle Xmas",
     "Diseño completo de Squirtle version navideña\n Bordado a mano",
     949.99,
-    "../../Pictures/Products/Coleccion_Navidad/Squirtle_Xmas.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Squirtle_Xmas.png");
 
 itemsController.addItem(
     "Arbol Navideño - Outline",
     "Diseño outline de arbol de navidad sencillo\n Estrella con bordado completo",
     449.99,
-    "../../Pictures/Products/Coleccion_Navidad/Xmas_outline.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Xmas_outline.png");
 
 itemsController.addItem(
     "Paloma de la Paz",
     "Paloma con diseño outline\n Corona navideña completa, bordado a mano",
     599.99,
-    "../../Pictures/Products/Coleccion_Navidad/Dove_Xmas.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Dove_Xmas.png");
 
 itemsController.addItem(
     "Reno con Corona",
     "Diseño completo de reno con corona navideña\n Bordado a mano",
     599.99,
-    "../../Pictures/Products/Coleccion_Navidad/Dow_Xmas.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Dow_Xmas.png");
 
 itemsController.addItem(
     "Cascanueces Completo",
     "Diseño completo de Cascanueces\n Bordado a mano",
     749.99,
-    "../../Pictures/Products/Coleccion_Navidad/Cascanueces_Nick.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Cascanueces_Nick.png");
 
 itemsController.addItem(
     "Santa Claus",
     "Deseño completo de Santa Claus\n Bordado a mano",
     599.99,
-    "../../Pictures/Products/Coleccion_Navidad/Santa_Isabelle.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Santa_Isabelle.png");
 
 itemsController.addItem(
     "Merry Christmas",
     "Diseño especial gengibre con corona navideña\n Bordado a mano",
     949.99,
-    "../../Pictures/Products/Coleccion_Navidad/Merry_Xmas.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Merry_Xmas.png");
 
 itemsController.addItem(
     "Muerdago - Outline",
     "Diseño de muerdago outline pequeño\n Bordado a mano",
     449.99,
-    "../../Pictures/Products/Coleccion_Navidad/muerdago.png");
+    "../../../Pictures/Products/Coleccion_Navidad/muerdago.png");
 
 itemsController.addItem(
     "Xmas - Town",
     "Diseño de villa navideña con detalles bordados\n Bordado a mano",
-    1499.99,
-    "../../Pictures/Products/Coleccion_Navidad/SuperXmas.png");
+    1995.99,
+    "../../../Pictures/Products/Coleccion_Navidad/SuperXmas.png");
 
 itemsController.addItem(
     "Xmas - Mexa",
     "Mexico Navideño - diseño completo\n Bordado a mano",
     749.99,
-    "../../Pictures/Products/Coleccion_Navidad/Xmas_MX.png");
+    "../../../Pictures/Products/Coleccion_Navidad/Xmas_MX.png");
 
 itemsController.addItem(
     "Corona - Nombre",
     "Corona completa con nombre\n Bordado a mano",
     749.99,
-    "../../Pictures/Products/PersonName/Muerdago.png");
+    "../../../Pictures/Products/PersonName/Muerdago.png");
 
 
 itemsController.addItem(
     "Xmas - Tree",
     "Arbolito Navideño con nombre\n Bordado a mano",
     749.99,
-    "../../Pictures/Products/PersonName/XmasTree.png");
+    "../../../Pictures/Products/PersonName/XmasTree.png");
 
 
 
